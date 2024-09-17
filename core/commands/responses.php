a7e7d27d6844d3f72bbafc507a0f2446bbf3d255
Atipat <exact@six4.dev>
2024-09-17 20:53:27 +0700

    🔧 Refactor response functions for better readability and maintainability
    Added separate functions for success, info, warning, and error messages. Created a function for handling not found responses.

--- core/commands/responses.php
+++ core/commands/responses.php
@@ -5,24 +5,45 @@ $commandSupport = [
     'new:model' => 'Create a new model file',
 ];
 
+/**
+ * @param string $message
+ * @return string
+ */
 function success(string $message): string
 {
     return "\e[42mSuccess\e[0m \e[32m : $message\e[39m\n";
 }
 
+/**
+ * @param string $message
+ * @return string
+ */
 function info(string $message): string
 {
     return "\e[44mInfo\e[0m \e[34m : $message\e[39m\n";
 }
+
+/**
+ * @param string $message
+ * @return string
+ */
 function warning(string $message): string
 {
     return "\e[43mWarning\e[0m \e[33m : $message\e[39m\n";
 }
+
+/**
+ * @param string $message
+ * @return string
+ */
 function error(string $message): string
 {
     return "\e[41mError\e[0m \e[31m : $message\e[39m\n";
 }
 
+/**
+ * @return string
+ */
 function notFound(): string
 {
     global $commandSupport;
